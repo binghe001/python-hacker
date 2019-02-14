cf27b2c8d10eaaee84c068e86d393abd606b3a7a
liuyazhuang <1028386804@qq.com>
2019-02-14 17:49:22 +0800

    新增分析PDF文件中的元数据信息脚本并添加脚本说明

--- com/binghe/hacker/tools/script/meta/meta_pdf.py
+++ com/binghe/hacker/tools/script/meta/meta_pdf.py
@@ -5,6 +5,8 @@
 # Created by 冰河
 # Description 获取PDF文件中的元数据信息
 #             本程序为下载：https://www.wired.com/images_blogs/threatlevel/2010/12/ANONOPS_The_Press_Release.pdf 文件进行分析
+#
+#             用法： python meta_pdf.py -F pdf文件
 # 博客 https://blog.csdn.net/l1028386804
 
 import pyPdf
