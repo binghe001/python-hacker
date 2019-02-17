dd9bbad43d9523127221f8267a5ad0f4498c36ae
liuyazhuang <1028386804@qq.com>
2019-02-17 14:58:38 +0800

    新增使用Dpkt发现下载LOIC的行为脚本并添加脚本说明

--- README.md
+++ README.md
@@ -71,6 +71,7 @@ QQ：2711098650
 4. analysis_pcap_dpkt.py: 使用dpkt分析pcap数据包
 5. analysis_pcap.py: 分析pcap数据包，可以直接看到数据包的源和目标的物理位置，此脚本是其他三个脚本的最终整合版
 6. analysis_pcap_kml.py: 分析pcap文件，并将分析结果生成KML文件在Google地球上标注
+7. analysis_dpkt_loic.py: 使用Dpkt发现下载LOIC的行为
 ```
 ### com.binghe.hacker.tools.script.scanner 包下的脚本
 ```
---
+++ com/binghe/hacker/tools/script/network/pcap/analysis_dpkt_loic.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- coding: gbk -*-
+# Date: 2019/2/17
+# Created by 冰河
+# Description 使用Dpkt发现下载LOIC的行为
+#             流程为：
+#                  解析HTTP流量，并检查其中有无通过HTTP GET获取压缩过的LOIC二进制可执行文件的情况，
+#                  如果HTTP层中使用了GET方法，则解析HTTP GET所要获取的统一资源标识符(URI)。如果
+#                  该URI所指向的文件的文件名中含有.zip和LOIC，则在屏幕上输出一条某个IP正在下载LOIC的消息
+#             Loic下载地址:http://sourceforge.net/projects/loic
+# 博客 https://blog.csdn.net/l1028386804
+
+import dpkt
+import socket
+import optparse
+
+#发现下载Loic的行为
+def findDownLoic(pcap):
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = socket.inet_ntoa(ip.src)
+            tcp = ip.data
+            http = dpkt.http.Request(tcp.data)
+            if http.method == 'GET':
+                uri = http.uri.lower()
+                if '.zip' in uri and 'loic' in uri:
+                    print '[!] ' + src + 'Downloaded LOIC.'
+        except:
+            pass
+
+#主函数
+def main():
+    parser = optparse.OptionParser("usage%prog -p <pcap file>")
+    parser.add_option('-p', dest='pcapFile', type='string', help='specify pcap file.')
+    (options, args) = parser.parse_args()
+    pcapFile = options.pcapFile
+    if pcapFile == None:
+        print parser.usage
+        exit(0)
+    f = open(pcapFile, 'r')
+    pcap = dpkt.pcap.Reader(f)
+    findDownLoic(pcap)
+
+#入口
+if __name__ == '__main__':
+    main()
+
+
+
