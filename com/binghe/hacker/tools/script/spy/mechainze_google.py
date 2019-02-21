2de35833d6774ffa27687e10cba563d40092e9d1
liuyazhuang <1028386804@qq.com>
2019-02-21 17:50:08 +0800

    新增从与Google API交互脚本并添加脚本说明

--- com/binghe/hacker/tools/script/spy/mechainze_google.py
+++ com/binghe/hacker/tools/script/spy/mechainze_google.py
@@ -11,23 +11,42 @@ from mechainze_browser import *
 import optparse
 import json
 
+#存储Google返回的结果数据
+class Google_Result:
+    def __init__(self, title, text, url):
+        self.title = title
+        self.text = text
+        self.url = url
+
+    def __repr__(self):
+        return self.title
+
 #使用Google搜索
 def google(search_item):
     ab = AnonBrowser()
     search_item = urllib.quote_plus(search_item)
     response = ab.open('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + search_item)
     objects = json.load(response)
-    print objects
+    results = []
+    for result in objects['responseData']['results']:
+        url = result['url']
+        title = result['titleNoFormatting']
+        text = result['content']
+        new_gr = Google_Result(title, text, url)
+        results.append(new_gr)
+    print results
 
 def main():
     parser = optparse.OptionParser('usage%prog -k <search keyword>')
-    parser.add_option('-c', dest='content', type='string', help='specify search content')
+    parser.add_option('-k', dest='content', type='string', help='specify search content')
     (options, args) = parser.parse_args()
     content = options.content
     if content == None:
         print parser.usage
         exit(0)
-    google(content)
+    else:
+        results = google(content)
+        print results
 
 if __name__ == '__main__':
     main()
